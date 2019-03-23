eec3bca236d5bfa70f44940794073763b669b74e
unknown <weeco91@gmail.com>
2019-03-23 10:04:10 +0100

    Add comments on exported storage request consts

--- kafka/storage_request.go
+++ kafka/storage_request.go
@@ -4,9 +4,14 @@ package kafka
 type StorageRequestType int
 
 const (
+	// StorageAddPartitionHighWaterMark is the request type to add a partition's high water mark
 	StorageAddPartitionHighWaterMark StorageRequestType = 0
-	StorageAddPartitionLowWaterMark  StorageRequestType = 1
-	StorageAddConsumerOffset         StorageRequestType = 2
+
+	// StorageAddPartitionLowWaterMark is the request type to add a partition's low water mark
+	StorageAddPartitionLowWaterMark StorageRequestType = 1
+
+	// StorageAddConsumerOffset is the request type to add a consumer's offset commit
+	StorageAddConsumerOffset StorageRequestType = 2
 )
 
 // StorageRequest is an entity to send requests to the storage module
