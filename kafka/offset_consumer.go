6e78c9baf2c06ef051e05812ed42c7cbbe83b8c9
unknown <weeco91@gmail.com>
2019-03-30 11:14:14 +0100

    Fix high water mark hack
    Clusters without any commited offset report water mark 0 on partitions

--- kafka/offset_consumer.go
+++ kafka/offset_consumer.go
@@ -136,7 +136,7 @@ func (module *OffsetConsumer) partitionConsumer(consumer sarama.Consumer, partit
 			offsetWaterMarks.Lock.RLock()
 			if val, exists := offsetWaterMarks.PartitionsByID[partitionID]; exists {
 				// Not sure why -1 is needed here, but otherwise there are lots of partition consumers with a remaining lag of 1
-				highWaterMark = val.HighWaterMark - 1
+				highWaterMark = int64(math.Min(0, float64(val.HighWaterMark-1)))
 			}
 			offsetWaterMarks.Lock.RUnlock()
 			if consumedOffset >= highWaterMark {
