c0f7ac65b848ee0a1f757759117fff0846653a18
Christian Ravn <christian.ravnen@gmail.com>
2021-11-03 14:48:02 +0100

    Create topic instead of panic, when error is UnknownTopicOrPartition

--- e2e/topic.go
+++ e2e/topic.go
@@ -27,14 +27,15 @@ func (s *Service) validateManagementTopic(ctx context.Context) error {
 		return fmt.Errorf("validateManagementTopic cannot get metadata of e2e topic: %w", err)
 	}
 
-	topicExists := false
-	if len(meta.Topics) == 1 {
-		err := kerr.ErrorForCode(meta.Topics[0].ErrorCode)
-		if err != nil {
-			return fmt.Errorf("failed to get metadata for end-to-end topic: %w", err)
-		}
+	// Error code 3 is UnknownTopicOrPartition, meaning that the topic does not exist.
+	// If the topic exists, but there's an error, then this should result in a fail
+	// When the topic doesn't exist, continue to create it
+	err_code := meta.Topics[0].ErrorCode
+	topicExists := err_code != 3
 
-		topicExists = true
+	if err_code != 0 && topicExists {
+		err := kerr.ErrorForCode(err_code)
+		return fmt.Errorf("failed to get metadata for end-to-end topic: %w", err)
 	}
 
 	// Create topic if it doesn't exist
